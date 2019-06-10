8c7cbe9de6988c49671f9756d303a98995391a9e
wjf18295788982 <48113114+wjf18295788982@users.noreply.github.com>
2019-06-10 10:35:32 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+  outline-style: none;
+}
+html,body,#app {
+    width: 100%;
+    height: 100%;
+    
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="wrapper">
+        <div class="header">
+            头部
+        </div>
+         <div class="main">
+            
+        </div>
+        <div class="footer">
+            尾部
+        </div>
+       
+    </div>
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="scss">
+    .wrapper{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        .header{
+            width: 100%;
+            height: 45px;
+            background: pink;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .main{
+            width: 100%;
+            flex: 1;
+        }
+         .footer{
+            width: 100%;
+            height: 45px;
+            background: pink;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+</style>
